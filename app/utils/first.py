4cab539d301bf4a216e2a4e02fe993e97420727e
Felipe Faria <felipe.faria@anovasistemas.com.br>
2025-05-10 19:28:26 -0300

    wip: getting fks

--- app/utils/first.py
+++ app/utils/first.py
@@ -3,4 +3,10 @@
 def get_first_kills_all_time(puuid: str, data: dict):
     rounds = data['data']['rounds']
 
-    
+    if rounds:
+        for round in rounds:
+            if round['players']['all_players']:
+                for player in round['players']['all_players']:
+                    if player['puuid'] == puuid:
+                        return player['first_kills']
+    return None
